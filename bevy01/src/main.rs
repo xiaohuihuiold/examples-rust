394fef6d5aceb814f19f3a725fd21fa6ec98eb5d
xiaohuihuiold <xiaohuihuiold@gmail.com>
2023-02-18 23:07:26 +0800

    feat(bevy01): 添加helloworld ecs

--- bevy01/src/main.rs
+++ bevy01/src/main.rs
@@ -1,3 +1,44 @@
+use bevy::app::{App, Plugin};
+use bevy::DefaultPlugins;
+use bevy::prelude::{Commands, Component, Query, Res, ResMut, Resource, With};
+use bevy::time::{Time, Timer, TimerMode};
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("A".to_string())));
+    commands.spawn((Person, Name("B".to_string())));
+    commands.spawn((Person, Name("V".to_string())));
+}
+
+fn great_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(great_people);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
